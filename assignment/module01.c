7ef1948a666a96216187937922b6ba1a959de2f2
Bhagirath Kher <123075549+bhagirathkher@users.noreply.github.com>
2023-02-22 09:59:00 +0530

    Add files via upload

---
+++ assignment/module01.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+    printf("name: Kher Bhagirath SanjayBhai");
+    printf("\n address - mangrol vadi vistar");
+    printf("\n birth: 05-08-2004");
+    printf("\n age: 19");
+}
