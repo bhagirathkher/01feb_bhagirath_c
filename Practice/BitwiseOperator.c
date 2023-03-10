a9f6adf9887fe4b1e75970a3505ce1c9f70df20b
Bhagirath Kher <123075549+bhagirathkher@users.noreply.github.com>
2023-03-10 11:36:46 +0530

    Add files via upload

---
+++ Practice/BitwiseOperator.c
@@ -0,0 +1,17 @@
+// Bitwise Operators :- & , ! , | , ~ , << , >>
+ṇ#include<stdio.h>
+void main()
+
+
+ {
+
+    int a ,b;
+    printf("Enter the value :");
+    scanf("%d%d",&a,&b);
+    printf("\nBitwise and is :%d",a&b);
+    printf("\nBitwise or is :%d",a|b):
+    printf("\nBitwise Left Shiftis :%d" ,a<<b);
+    printf("\nBitwise Right shift is :%d" ,a>>b );
+    printf("\nBitwise !is :%d" ,!(a|b));
+
+ }
