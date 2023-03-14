721c5c1b80f18b7b8e7726e782bcc574dc117661
Bhagirath Kher <123075549+bhagirathkher@users.noreply.github.com>
2023-03-14 12:20:53 +0530

    Add files via upload

---
+++ Module-2/Assignment/SquareandCube.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+ int x;
+
+ printf("Enter a number");
+
+ scanf("%d",&x);
+
+ printf("\nSquare of the number %d is %d",x,x*x);
+
+ printf("\nCube of the number %d is %d",x,x*x*x);
+
+ return 0;
+
+}
